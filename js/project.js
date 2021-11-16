e6581e1cfe230a4d280dd015028c1f8ecf8c51b2
Vusal Huseynov <87518350+huseynovvusal@users.noreply.github.com>
2021-11-16 22:19:57 +0400

    Delete js directory

--- js/project.js
+++
@@ -1,6 +0,0 @@
-function toggle() {
-  document.querySelector(".hamburger").classList.toggle("hamburger-active");
-  document.querySelector("ul").classList.toggle("list-active");
-}
-
-document.querySelector(".hamburger").addEventListener("click", toggle);
