e8f9011ceedf2604b7fbf1cf491b064c9d0e80a9
Vusal Huseynov <87518350+huseynovvusal@users.noreply.github.com>
2021-11-18 22:35:43 +0400

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,46 @@
+class Background {
+  constructor(background, time) {
+    this.background = background;
+    this.time = time;
+  }
+
+  circle() {
+    let span = document.createElement("span");
+
+    let left = Math.random() * window.innerWidth;
+    let top = Math.random() * window.innerHeight;
+    let scale = Math.random() * 40;
+
+    span.style.left = `${left}px`;
+    span.style.top = `${top}px`;
+    span.style.width = `${scale}px`;
+    span.style.height = `${scale}px`;
+
+    span.classList.add("background-circle");
+
+    document.querySelector(".background").appendChild(span);
+
+    setTimeout(() => {
+      span.remove();
+    }, 3000);
+  }
+
+  mouseMove(e) {
+    let mouseX = -(e.clientX * 0.01);
+    let mouseY = -(e.clientY * 0.01);
+
+    let bg = document.querySelector(".background");
+
+    bg.style.left = `${mouseX}px`;
+    bg.style.top = `${mouseY}px`;
+    bg.style.transform = `scale(${-mouseX * 0.05 + 1})`;
+  }
+}
+
+const bg = new Background(document.querySelector(".background"), 3000);
+
+setInterval(bg.circle, 300);
+
+document.addEventListener("mousemove", (e) => {
+  bg.mouseMove(e);
+});
---
+++ js/project.js
@@ -0,0 +1,23 @@
+function toggle() {
+  document.querySelector(".hamburger").classList.toggle("hamburger-active");
+  document.querySelector("ul").classList.toggle("list-active");
+  document.querySelector(".content").classList.toggle("content-disable");
+  document.querySelector("nav h2").classList.toggle("content-disable-2");
+}
+
+document.querySelector(".hamburger").addEventListener("click", toggle);
+
+window.addEventListener("resize", () => {
+  document.querySelector(".hamburger").classList.remove("hamburger-active");
+  document.querySelector("ul").classList.remove("list-active");
+  document.querySelector(".content").classList.remove("content-disable");
+  document.querySelector("nav h2").classList.remove("content-disable-2");
+});
+
+document.querySelectorAll(".comment-btn").forEach((el) => {
+  el.addEventListener("click", () => {
+    el.parentElement.parentElement
+      .querySelector(".comment")
+      .classList.toggle("comment-active");
+  });
+});
